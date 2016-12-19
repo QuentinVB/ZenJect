bd130eee6f9b3b55aea74e06d018760ef05d3e7f
KaentinSaphire <kentinfl@orange.fr>
2016-12-19 02:16:35 +0100

    commiting the base system for testing zenject

---
+++ Assets/TestInstaller.cs
@@ -0,0 +1,20 @@
+﻿using Zenject;
+using UnityEngine;
+using System.Collections;
+
+public class TestInstaller : MonoInstaller
+{
+    public override void InstallBindings()
+    {
+        Container.Bind<string>().FromInstance("Hello World!");
+        Container.Bind<TestRunner>().NonLazy();
+    }
+}
+
+public class TestRunner
+{
+    public TestRunner(string message)
+    {
+        Debug.Log(message);
+    }
+}
